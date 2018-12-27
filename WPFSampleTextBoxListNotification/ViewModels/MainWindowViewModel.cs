ee9e70407292c1e5f7208e338413ca7286a167c7
shuntaro4 <shuntaro4@gmail.com>
2018-12-28 03:03:52 +0900

    update: MainWindow のタイトル初期値を修正

--- WPFSampleTextBoxListNotification/ViewModels/MainWindowViewModel.cs
+++ WPFSampleTextBoxListNotification/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace WPFSampleTextBoxListNotification.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "WPF Sample App";
         public string Title
         {
             get { return _title; }
