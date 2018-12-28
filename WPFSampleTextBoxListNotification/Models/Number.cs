2de6dadc078dfcc407fa55e78319616ee1503edc
shuntaro4 <shuntaro4@gmail.com>
2018-12-28 17:47:47 +0900

    add: Number クラスを追加

---
+++ WPFSampleTextBoxListNotification/Models/Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WPFSampleTextBoxListNotification.Models
+{
+    public class Number : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private int _amount;
+        public int Amount
+        {
+            get => _amount;
+            set
+            {
+                _amount = value;
+                OnPropertyChanged(nameof(Amount));
+
+                _amountChangedNotification?.Invoke();
+            }
+        }
+
+        private Action _amountChangedNotification = null;
+
+        public Number(int amount, Action amountChangedNotification = null)
+        {
+            Amount = amount;
+            _amountChangedNotification = amountChangedNotification;
+        }
+    }
+}
